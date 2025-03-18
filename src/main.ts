3e9a9c85127ee907ee5625f6104aed347d71066a
Inplex-sys <anthill-spud-puma@duck.com>
2025-03-18 12:55:36 +0100

    Add TypeScript support and middleware for calculated value in Express app

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,22 @@
-const express = require("express");
+import express from "express";
+
+declare global {
+	namespace Express {
+		interface Request {
+			calculatedValue?: number;
+		}
+	}
+}
+
 const app = express();
 const port = 8888;
+
+app.use((req, res, next) => {
+	const result = 4 * 7;
+	req.calculatedValue = result;
+	console.log(`Calculated value: ${result}`);
+	next();
+});
 
 app.get("/", (req, res) => {
 	res.send("Hello World!");
